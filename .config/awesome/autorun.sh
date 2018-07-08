c9a227f43259c49a0742ad92dc3cec7ef0e19def
RaymondBaker <raymond_baker@hotmail.com>
2018-07-08 00:33:43 -0400

    removed chrome logs

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -10,7 +10,8 @@ run dbus-launch update-checker
 run light-locker
 run pulseaudio -D
 run compton --shadow-exclude '!focused'
-run xcape -e 'Super_L=Super_L|Shift_L|p'
+# This runs rofi on super
+#run xcape -e 'Super_L=Super_L|Shift_L|p'
 run /usr/lib/mate-polkit/polkit-mate-authentication-agent-1
 run thunar --daemon
 run xfce4-power-manager
