02ce387db85645193ed64bb020cc6c38ed7ce0f0
RaymondBaker <40651354+RaymondBaker@users.noreply.github.com>
2019-05-09 10:36:36 -0400

    link .tmux.conf

--- setup.sh
+++ setup.sh
@@ -2,14 +2,16 @@
 
 #TODO: add options for what system this is going on ie putty awesome
 
-rm ~/.zprofile
-rm ~/.vimrc
-rm ~/.zshrc
+rm    ~/.zprofile
+rm    ~/.vimrc
+rm    ~/.zshrc
+rm    ~/.tmux.conf
 rm -r ~/.config/awesome
 rm -r ~/.config/ranger
 
-ln -s ~/.dotfiles/.zprofile ~/.zprofile
-ln -s ~/.dotfiles/.vimrc    ~/.vimrc
-ln -s ~/.dotfiles/.zshrc    ~/.zshrc
-ln -s ~/.dotfiles/.awesome  ~/.config/awesome
-ln -s ~/.dotfiles/.ranger   ~/.config/ranger
+ln -s ~/.dotfiles/.zprofile  ~/.zprofile
+ln -s ~/.dotfiles/.vimrc     ~/.vimrc
+ln -s ~/.dotfiles/.zshrc     ~/.zshrc
+ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/.awesome   ~/.config/awesome
+ln -s ~/.dotfiles/.ranger    ~/.config/ranger
