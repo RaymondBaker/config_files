a10579b27467a5c9d0b709cbc1c26ddbf155bc46
RaymondBaker <raymond_baker@hotmail.com>
2019-11-17 21:59:05 -0500

    added surf

--- setup.sh
+++ setup.sh
@@ -7,9 +7,10 @@ rm -r ~/.config/awesome
 rm -r ~/.config/ranger
 rm -r ~/.newsboat
 
-ln -s ~/.dotfiles/.newsboat ~/.newsboat
-ln -s ~/.dotfiles/.zprofile ~/.zprofile
-ln -s ~/.dotfiles/.vimrc    ~/.vimrc
-ln -s ~/.dotfiles/.zshrc    ~/.zshrc
-ln -s ~/.dotfiles/.awesome  ~/.config/awesome
-ln -s ~/.dotfiles/.ranger   ~/.config/ranger
+ln -s ~/.dotfiles/.newsboat         ~/.newsboat
+ln -s ~/.dotfiles/.zprofile         ~/.zprofile
+ln -s ~/.dotfiles/.vimrc            ~/.vimrc
+ln -s ~/.dotfiles/.zshrc            ~/.zshrc
+ln -s ~/.dotfiles/.awesome          ~/.config/awesome
+ln -s ~/.dotfiles/.ranger           ~/.config/ranger
+ln -s ~/.dotfiles/surf/script.js    ~/.surf/script.js
