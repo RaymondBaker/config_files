15500fea3c9b3457a0033986f886aa584fc1a0dd
Raymond Baker <raymond.baker@ibm.com>
2025-03-20 09:10:28 -0400

    merge work and home computer

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set -gx EDITOR /usr/bin/nvim
+set -gx EDITOR nvim
+
+fish_add_path ~/.local/bin
+
+direnv hook fish | source
