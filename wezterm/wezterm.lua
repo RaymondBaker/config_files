51d07c1797703a8c2d6d967eb95c399e188fe5a8
Raymond Baker <raymond.baker@ibm.com>
2025-03-20 10:43:09 -0400

    working with smart_splits

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,7 +7,10 @@ local config = wezterm.config_builder()
 
 config.color_scheme = 'Gruvbox Dark (Gogh)'
 config.automatically_reload_config = true
-config.window_decorations = "RESIZE"
+config.window_decorations = 'RESIZE'
+
+config.default_cursor_style = 'BlinkingBlock'
+config.animation_fps = 64
 
 -- Required because gnome wayland requires apps to draw their title bar and default is really ugly
 config.enable_wayland = false
@@ -15,18 +18,38 @@ config.enable_wayland = false
 config.font_size = 13
 
 config.keys = {
-  -- This will create a new split and run your default program inside it
   {
-    key = '\\',
+    key = '-',
     mods = 'CTRL',
     action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
   },
   {
-    key = '-',
+    key = '\\',
     mods = 'CTRL',
     action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
   },
+  {
+    key = 'x',
+    mods = 'CTRL',
+    action = wezterm.action.CloseCurrentPane { confirm = true },
+  },
 }
+
+-- Must happen after keys is set
+local smart_splits = wezterm.plugin.require('https://github.com/mrjones2014/smart-splits.nvim')
+smart_splits.apply_to_config(config, {
+  direction_keys = {
+    move = { 'h', 'j', 'k', 'l' },
+    resize = { 'LeftArrow', 'DownArrow', 'UpArrow', 'RightArrow' },
+  },
+  -- modifier keys to combine with direction_keys
+  modifiers = {
+    move = 'META', -- modifier to use for pane movement, e.g. CTRL+h to move left
+    resize = 'META', -- modifier to use for pane resize, e.g. META+h to resize to the left
+  },
+  -- log level to use: info, warn, error
+  log_level = 'info',
+})
 
 -- The filled in variant of the < symbol
 local SOLID_LEFT_ARROW = wezterm.nerdfonts.pl_right_hard_divider
